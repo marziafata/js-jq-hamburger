447d5fa63dee2bcafb0abf1a7cfd18f5bd615447
Marzia Fatarella <marziafatarella@gmail.com>
2020-04-29 15:57:53 +0200

    ho fatto due metodi

---
+++ main.js
@@ -0,0 +1,32 @@
+// L'esercitazione di oggi prevede di aggiungere il codice js necessario per visualizzare l'hamburger menu su mobile, a partire da html e css già pronti.
+
+// body > header > div.header-right > a > i
+
+
+// metodo 1: funziona! ^___^ Feliciona!
+// click su menu hamburger e compare tendina
+var hamburger = $(".fa-bars")
+
+hamburger.click(function() {
+    $(".hamburger-menu").show("fast");
+
+})
+
+// quando clicco sulla X scompare
+$(".fa-times").click(function() {
+    $(".hamburger-menu").hide("fast");
+
+})
+
+
+// // metodo 2: funziona! ^___^ Feliciona!
+// // click su menu hamburger e compare tendina
+// var hamburger = $(".fa-bars")
+//
+// hamburger.click(function() {
+//     $(".hamburger-menu").addClass("show");
+// })
+// // quando clicco sulla X scompare
+// $(".fa-times").click(function() {
+//     $(".hamburger-menu").removeClass("show");
+// })
